07e57f43f1b65189bf6f0b1640b3abeb02295df2
Austin <robbi18@msu.edu>
2017-12-06 12:12:55 -0500

    main comments

--- src/main/java/etsCodeChallengeGradle/Main.java
+++ src/main/java/etsCodeChallengeGradle/Main.java
@@ -15,11 +15,17 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*
+            creates two decks, prints one out to make sure all cards have been
+            created and can be dealt. Shuffles the second one and then prints
+            it out to make sure the shuffle sufficiently randomizes the card
+            order
+        */
         deck d = new deck();
         deck d2 = new deck();
         int count = 0;
         System.out.println("Deck 1:");
-        for(int i = 0; i<52; i++){
+        for(int i = 0; i<53; i++){
             card c = d.dealOneCard();
             if(c != null){
                 String s = c.getSuit() + " : " + c.getVal() + " : " + count;
@@ -32,7 +38,7 @@ public class Main {
         
         d2.shuffle();
         count = 0;
-        for(int i = 0; i<52; i++){
+        for(int i = 0; i<53; i++){
             card c = d2.dealOneCard();
             if(c != null){
                 String s = c.getSuit() + " : " + c.getVal() + " : " + count;
